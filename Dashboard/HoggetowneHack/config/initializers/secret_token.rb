868f9b6555e3f7a33ee92851ceac2600a36429d6
devanp92 <devanp92@ufl.edu>
2014-02-22 13:21:14 -0500

    Adding ruby project

---
+++ Dashboard/HoggetowneHack/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoggetowneHack::Application.config.secret_key_base = '80b659cc911b3519a7347d46d9db3969f3bc19304424990130f08b4485b6cd8400330618e7181ab0948a039dd430d9a26f55b0093429d3496965ad36b80ebaf6'
---
+++ Dashboard/HoggetowneHack/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HoggetowneHack::Application.config.session_store :cookie_store, key: '_HoggetowneHack_session'
